5b6f2b7a42f1a88bc22dd423f77f57be108132e5
Test <sruthirajk55@gmail.com>
2022-02-14 18:04:52 +0530

    modified commit

--- Amazon/src/main/java/testScenarios/Test_Amazon.java
+++ Amazon/src/main/java/testScenarios/Test_Amazon.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import objects.AmazonHomePage;
 import objects.AmazonProductPage;
+import objects.LoginAmzon;
 
 public class Test_Amazon {
 
@@ -23,15 +24,29 @@ public class Test_Amazon {
 		driver.manage().window().maximize();
 		driver.navigate().to(url);
 	}
+	@Test(priority = 0)
+	@Parameters("email")
+	public void email(String email) {
+		LoginAmzon   obj = new LoginAmzon(driver);
+		obj.em(email);
+	}
+
+	@Test(priority = 1)
+	@Parameters("Password")
+	public void password(String Password) {
+		LoginAmzon	obj = new LoginAmzon(driver);
+		obj.psw(Password);
+	}
+		
 	
-	@Test
+	@Test(priority = 2)
 	@Parameters("search")
 	public void HomePageOperation1(String search) {
 		AmazonHomePage page1=new AmazonHomePage(driver);
 		page1.SearchInAmazon(search);
 	}
-	
-	@Test
+
+	@Test(priority = 3)
 	public void HomePageOperation2() {
 //		AmazonHomePage page2=new AmazonHomePage(driver);
 //		page2.selectProduct();
@@ -39,7 +54,8 @@ public class Test_Amazon {
 		objectrepo.selectProduct();
 	}
 	
-	@Test
+	
+	@Test(priority = 4)
 	public void addToCartOperation() {
 		AmazonProductPage page2=new AmazonProductPage(driver);
 		page2.addToCart();
